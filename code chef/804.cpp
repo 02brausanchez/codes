21d580f15b62ea3bb18cc060ba75bab21cc2e806
besm <120343735+02brausanchez@users.noreply.github.com>
2024-07-05 13:58:05 -0600

    Update 804.cpp

--- code chef/804.cpp
+++ code chef/804.cpp
@@ -1,68 +1,34 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 400005;
-
-vector<int> adj[MAXN];
-int color[MAXN]; // 0 means uncolored, 1 and 2 represent two different colors
-
-bool bfs(int start, int n) {
-    queue<int> q;
-    q.push(start);
-    color[start] = 1;
-
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-
-        for (int v : adj[u]) {
-            if (color[v] == 0) {
-                color[v] = (color[u] == 1) ? 2 : 1;
-                q.push(v);
-            } else if (color[v] == color[u]) {
-                return false; // Found an odd cycle
-            }
-        }
-    }
-
-    return true;
-}
+map<string, char> binToChar = {
+    {"00", 'A'},
+    {"01", 'T'},
+    {"10", 'C'},
+    {"11", 'G'}
+};
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-
-    vector<pair<int, int>> animals(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> animals[i].first >> animals[i].second;
-    }
-
-    for (int i = 0; i < m; ++i) {
-        int x, y;
-        cin >> x >> y;
-        adj[x].push_back(y);
-        adj[y].push_back(x);
-    }
-
-    // Check if the graph is bipartite
-    bool possible = true;
-    for (int i = 1; i <= n; ++i) {
-        if (color[i] == 0) {
-            if (!bfs(i, n)) {
-                possible = false;
-                break;
-            }
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        
+        string result;
+        for (int i = 0; i < n; i += 2) {
+            string binPair = s.substr(i, 2);
+            result += binToChar[binPair];
         }
+        
+        cout << result << "\n";
     }
-
-    if (possible) {
-        cout << "possible" << endl;
-    } else {
-        cout << "impossible" << endl;
-    }
-
+    
     return 0;
 }
+
 
