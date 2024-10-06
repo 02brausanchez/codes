8ecb93452dbd10af6849e245d46e634fba47dc02
braulio <120343735+02brausanchez@users.noreply.github.com>
2024-10-06 01:01:51 -0600

    Update 7171.cpp

--- omega up/7171.cpp
+++ omega up/7171.cpp
@@ -9,38 +9,34 @@ int main() {
     int n; 
     cin >> n;
     
-    vector<int> Array(n); // Usamos un vector en lugar de un arreglo estático
+    vector<int> Array(n); 
     
     for(int i = 0; i < n; i++) {
         cin >> Array[i];
     }
-    
-    // Encontramos el máximo valor en el arreglo
+
     int max_valor = Array[0];
     for(int i = 1; i < n; i++) {
         if(Array[i] > max_valor) {
             max_valor = Array[i];
         }
     }
-    
-    // Contamos cuántos números son estrictamente menores que max_valor
+
     int cont_min = 0;
     for(int i = 0; i < n; i++) {
         if(Array[i] < max_valor) {
             cont_min++;
         }
     }
-    
-    // Imprimimos cuántos números cumplen la condición
+
     cout << cont_min << endl;
-    
-    // Imprimimos los números que son estrictamente menores que max_valor
+
     for(int i = 0; i < n; i++) {
         if(Array[i] < max_valor) {
             cout << Array[i] << " ";
         }
     }
-    cout << endl;
+    cout << "\n";
     
     return 0;
 }
