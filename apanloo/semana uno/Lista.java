33428e0dae2a3c5ecb0dd3a789c0f976cf228b10
braulio <120343735+02brausanchez@users.noreply.github.com>
2024-10-27 23:17:05 -0600

    Update Lista.java

--- apanloo/semana uno/Lista.java
+++ apanloo/semana uno/Lista.java
@@ -2,23 +2,23 @@ package semanauno;
 
 public class Lista {
     Nodo l;
-    Nodo ultimo;
+    //Nodo ultimo;
     public Lista (){
         l = null;
-        ultimo = null;
+        //ultimo = null;
     }
     //De manera no recursiva
     public void agregarNodoFinal(int info){
         if(l == null){
             l = new Nodo(info);
-            ultimo = l;
+            //ultimo = l;
         }else {
             Nodo aux;
             for (aux = l; aux.sig != null; aux = aux.sig) {
 
             }
             aux.sig = new Nodo(info);
-            ultimo = aux.sig;
+            //ultimo = aux.sig;
         }
     }
 
@@ -26,9 +26,28 @@ public class Lista {
     @Override
     public String toString(){
         return "[" + (ultimo != null ? ultimo.info : "") + "]";
+        /*if(ultimo != null){
+            return "[" + ultimo.info + "]"; 
+        }else{
+            return "[]";
+        }*/    
     }
-
-    /* De manera recursiva
+    
+    //Imprime todos los valores
+    @Override
+    public String toString() {
+        String valores = "";
+        Nodo aux;
+        for (aux = l; aux != null; aux = aux.sig) {
+            valores += aux.info;
+            if (aux.sig != null) {
+                valores += ",";
+            }
+        }
+        return "[" + valores + "]";
+    }  
+     
+    //De manera recursiva
     private Nodo insertaFinal(Nodo a, int info){
         if(a == null){return new Nodo(info);}
         a.sig = insertaFinal(a.sig, info);
@@ -37,7 +56,5 @@ public class Lista {
 
     public void insertaFinal(int info){
         l = insertaFinal(l,info);
-    }*/
-
-
+    }
 }
