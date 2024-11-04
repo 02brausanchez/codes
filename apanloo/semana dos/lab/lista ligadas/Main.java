79e7e1e9269f2275bd2d4572b1230dce66d76a66
braulio <120343735+02brausanchez@users.noreply.github.com>
2024-11-03 21:01:55 -0600

    Add files via upload

---
+++ apanloo/semana dos/lab/lista ligadas/Main.java
@@ -0,0 +1,25 @@
+package apanloo;
+
+public class Main {
+	public static void main(String[] args) {
+		Listas l = new Listas(); 
+		/*l.agregarFinalR(1);
+		l.agregarFinalR(2);
+		l.agregarFinalR(3);
+		System.out.println(l.numNodosR());
+		*/ 
+		
+		l.insertaOrdenadoR(1);
+		l.insertaOrdenadoR(3);
+		l.insertaOrdenadoR(2);
+		l.insertaOrdenadoR(5);
+		l.insertaOrdenadoR(6);
+		l.insertaOrdenadoR(0);
+		l.insertaOrdenadoR(8);
+		System.out.println("lista original: " + l);
+
+		l.invierteR(); 
+		System.out.println("lista invertida: " + l);
+		
+	}
+}
---
+++ apanloo/semana dos/lab/lista ligadas/Nodo.java
@@ -0,0 +1,16 @@
+package apanloo;
+
+public class Nodo {
+	int info; 
+	Nodo sig; 
+	
+	public Nodo(int info) {
+		this.info = info; 
+		this.sig = null; 
+	}
+	
+	@Override 
+	public String toString() {
+		return "[" + info + "]"; 
+	}
+}
