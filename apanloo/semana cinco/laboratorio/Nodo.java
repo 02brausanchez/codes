3d51776da319dafecb450280de1831b684449307
braulio <120343735+02brausanchez@users.noreply.github.com>
2024-11-18 10:19:48 -0600

    Add files via upload

---
+++ apanloo/semana cinco/laboratorio/Nodo.java
@@ -0,0 +1,14 @@
+package PracticaCuatro;
+
+public class Nodo {
+    Nodo hizq,hder;
+    char info;
+    public Nodo(char info){
+        this.info = info;
+        hizq = hder = null;
+    }
+    @Override
+    public String toString(){
+        return "" + info;
+    }
+}
