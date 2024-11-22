d854569e9f7f3ca2a2995440c575678b96d1609f
braulio <120343735+02brausanchez@users.noreply.github.com>
2024-11-21 22:48:03 -0600

    Add files via upload

---
+++ apanloo/semana cinco/lab2/ArbolMain.java
@@ -0,0 +1,37 @@
+package PracticaCuatro;
+
+public class ArbolMain {
+    public static void main(String[] args) {
+        Arbol a = new Arbol();
+        //System.out.println(a);
+        //Arbol b = new Arbol();
+        a = a.ejemplo1();
+       // b = b.ejemplo2();
+        System.out.println(a.completo());
+
+
+        /*
+        System.out.println("Recorrido previo:");
+        b.previo();
+
+        System.out.println("Recorrido en orden:");
+        b.enOrden();
+
+        System.out.println("Recorrido postorden:");
+        b.postOrden();
+
+        System.out.println("Número de nodos: " + b.numNodo());
+
+        System.out.println("Hojas del árbol:");
+        b.soloHojas();
+
+        System.out.println("Nodos no hoja:");
+        b.noHojas();
+
+        System.out.println("Altura del árbol: " + b.altura());
+        System.out.println("Hay nodos iguales: " + b.iguales(b));
+        System.out.println("Representación del árbol:");
+        System.out.println(b);
+        */
+    }
+}
