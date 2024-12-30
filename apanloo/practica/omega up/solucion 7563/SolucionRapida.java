6a5625406e806e4d0adfe4b179d16e76ca68f6a4
braulio <120343735+02brausanchez@users.noreply.github.com>
2024-12-30 03:06:57 -0600

    Update SolucionRapida.java

--- apanloo/practica/omega up/solucion 7563/SolucionRapida.java
+++ apanloo/practica/omega up/solucion 7563/SolucionRapida.java
@@ -17,23 +17,20 @@ import java.util.StringTokenizer;
             private byte[] buffer;
             private int bufferPointer, bytesRead;
 
-            public Reader()
-            {
+            public Reader(){
                 din = new DataInputStream(System.in);
                 buffer = new byte[BUFFER_SIZE];
                 bufferPointer = bytesRead = 0;
             }
 
-            public Reader(String file_name) throws IOException
-            {
+            public Reader(String file_name) throws IOException{
                 din = new DataInputStream(
                         new FileInputStream(file_name));
                 buffer = new byte[BUFFER_SIZE];
                 bufferPointer = bytesRead = 0;
             }
 
-            public String readLine() throws IOException
-            {
+            public String readLine() throws IOException{
                 byte[] buf = new byte[64]; // line length
                 int cnt = 0, c;
                 while ((c = read()) != -1) {
@@ -50,8 +47,7 @@ import java.util.StringTokenizer;
                 return new String(buf, 0, cnt);
             }
 
-            public int nextInt() throws IOException
-            {
+            public int nextInt() throws IOException{
                 int ret = 0;
                 byte c = read();
                 while (c <= ' ') {
@@ -69,8 +65,7 @@ import java.util.StringTokenizer;
                 return ret;
             }
 
-            public long nextLong() throws IOException
-            {
+            public long nextLong() throws IOException{
                 long ret = 0;
                 byte c = read();
                 while (c <= ' ')
@@ -86,8 +81,7 @@ import java.util.StringTokenizer;
                 return ret;
             }
 
-            public double nextDouble() throws IOException
-            {
+            public double nextDouble() throws IOException{
                 double ret = 0, div = 1;
                 byte c = read();
                 while (c <= ' ')
@@ -111,23 +105,20 @@ import java.util.StringTokenizer;
                 return ret;
             }
 
-            private void fillBuffer() throws IOException
-            {
+            private void fillBuffer() throws IOException{
                 bytesRead = din.read(buffer, bufferPointer = 0,
                         BUFFER_SIZE);
                 if (bytesRead == -1)
                     buffer[0] = -1;
             }
 
-            private byte read() throws IOException
-            {
+            private byte read() throws IOException{
                 if (bufferPointer == bytesRead)
                     fillBuffer();
                 return buffer[bufferPointer++];
             }
 
-            public void close() throws IOException
-            {
+            public void close() throws IOException{
                 if (din == null)
                     return;
                 din.close();
