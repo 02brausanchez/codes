4cc20904ec40107485514989a3464f5ab3395c39
braulio <120343735+02brausanchez@users.noreply.github.com>
2025-01-22 16:48:55 -0600

    Update ControlAsistencia.java

--- apanloo/practica/ControlAsistencia.java
+++ apanloo/practica/ControlAsistencia.java
@@ -6,21 +6,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ControlAsistencia {
-    /*
-    public static String []  asistencia(String name){
-        String controlAsistencia = new String [name]; 
-        int count = 0; 
-
-        for(int i = 0; i < name.length(); i++){
-            guardaNombres[i]; 
-            count++; 
-        }
-
-        for(String x: ){
-            System.out.println();
-        }
-    } */
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         HashMap<String, Integer> asistencias = new HashMap<>(); 
