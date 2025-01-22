e47304bf1a35498b00c041dc04a86f491b1e698c
braulio <120343735+02brausanchez@users.noreply.github.com>
2025-01-22 16:52:54 -0600

    Add files via upload

---
+++ apanloo/segundo parcial/problema tres/C.java
@@ -0,0 +1,12 @@
+package ProgramaCuatro;
+
+public class C extends B {
+    public C(int a) {
+        super(a);
+        System.out.println("constructor de C entero ");
+    }
+
+    public C() {
+        System.out.println("soy el constructor de C ");
+    }
+}
