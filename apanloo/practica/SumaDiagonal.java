f00e911507026aa4b88e898e645a80456b7d96f6
braulio <120343735+02brausanchez@users.noreply.github.com>
2025-02-10 14:42:17 -0600

    Update SumaDiagonal.java

--- apanloo/practica/SumaDiagonal.java
+++ apanloo/practica/SumaDiagonal.java
@@ -1,22 +1,6 @@
 package Practica;
 
 public class SumaDiagonal {
-    /*  Problema:
-        Hay que hacer una matriz de n x m
-        es decir hay que considerar las matrices cuadradas
-        en donde se tendra que sumar sus diagonales.
-        Ejemplo:
-        Dada una matriz A de 2 x 2:
-        {[1,2],[1,2]}^t
-        1 2
-        1 2 -> la suma en digonal: 3
-        otro ejemplo:
-        Dada una matriz de 3 x 3:
-        1 2 3
-        4 5 6
-        7 8 9 -> suma en diagonal: 15
-     */
-
     public static int sumaDiagonalM(int [][] matriz){
         //Se tendra que recorre la matriz
         int suma = 0;
